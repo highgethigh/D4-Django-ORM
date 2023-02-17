010cd06f9777999f5db29a9aa20e1723971ceb0c
Artem <87155286+highgethigh@users.noreply.github.com>
2023-02-17 03:25:35 +0300

    Added path for search news

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 # импортируем наше представление
 from .views import PostList, PostDetail
+from . import views
 
 urlpatterns = [
     # # т.к. сам по себе это класс, то нам надо представить этот класс в виде view.
     # Для этого вызываем метод as_view
     path('', PostList.as_view()),
-    path('<int:pk>', PostDetail.as_view()),  # pk — это первичный ключ новости, который будет выводиться у нас в шаблон
+    path('<int:pk>', PostDetail.as_view(), name='post'),  # pk — это первичный ключ новости, который будет выводиться у нас в шаблон
+    path('search/', views.search, name='search'),
 ]
 
 
