aef47efcad9c887ee8a83fcb4522452ce7fab29c
Artem <87155286+highgethigh@users.noreply.github.com>
2023-02-18 19:44:51 +0300

    Added filter for search (GET-request)

--- NewsPaper/news/search.py
+++ NewsPaper/news/search.py
@@ -4,18 +4,22 @@ from .models import Post
 class PostFilter(FilterSet):
     class Meta:
         model = Post
-        fields = {
-            # по названию заголовка
-            'title': ['icontains'],
-            # позже какой-либо даты
-            'post_data': ['gt'],
-            # по имени пользователя автора
-            'post_link_author': ['exact'], # мы хотим чтобы нам выводило имя хотя бы отдалённо похожее на то, что запросил пользователь
+        fields = [ 'title', 'post_data', 'post_link_author', 'categoryType', 'text']
 
-            'text': ['icontains'],
-        }
 
-        # либо fields = [ 'title', 'post_data', 'post_link_author', 'text']
+
+        # либо fields = [ 'title', 'post_data', 'post_link_author', 'categoryType', 'text']
         # способ легче, без указаний значений ['icontains'], ['gt'], ['exact'], ['icontains'],
 
-        
+        # fields = {
+            # по названию заголовка
+#           'title': ['icontains'],
+#           # позже какой-либо даты
+#           'post_data': ['gt'],
+#           # по имени пользователя автора
+#           'post_link_author': ['exact'], # мы хотим чтобы нам выводило имя хотя бы отдалённо похожее на то, что запросил пользователь
+#
+#           'categoryType': ['exact'],
+#
+#           'text': ['icontains'],
+#        }
