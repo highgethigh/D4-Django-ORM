864b7afa67f2dc118b71888af0735a4f67ce12f5
Artem <87155286+highgethigh@users.noreply.github.com>
2023-02-18 19:30:55 +0300

    Added form for POST-request

---
+++ NewsPaper/news/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm, BooleanField
+from .models import Post
+
+
+# создаём модельную форму
+class PostForm(ModelForm):
+    check_box = BooleanField(label='Галочка')  # добавляем галочку, или же true-false поле
+
+    class Meta:
+        model = Post  # это модель, по которой будет строиться форма
+
+        # поля, которые будут выводиться на страничке
+        fields = ['title', 'post_link_author', 'post_link_category', 'text', 'check_box']
