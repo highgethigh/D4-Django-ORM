b2167c5c8a21f64f6dc6159fe21b7e88268efa5c
Artem <87155286+highgethigh@users.noreply.github.com>
2023-02-18 19:47:25 +0300

    added new urls

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # импортируем наше представление
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostCreateView, PostDeleteView, PostUpdateView
 from . import views
 
 urlpatterns = [
@@ -9,6 +9,9 @@ urlpatterns = [
     path('', PostList.as_view()),
     path('<int:pk>', PostDetail.as_view(), name='post'),  # pk — это первичный ключ новости, который будет выводиться у нас в шаблон
     path('search/', views.search, name='search'),
+    path('add/', PostCreateView.as_view(), name='post_create'), # ссылка на создание новости/статьи
+    path('news/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('news/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
 ]
 
 
